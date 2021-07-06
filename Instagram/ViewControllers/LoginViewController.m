f5e654b45354e86ff5709424a846ae984aec4799
athomas9195 <anna.tina.thomas@gmail.com>
2021-07-06 14:11:47 -0700

    login signup

---
+++ Instagram/ViewControllers/LoginViewController.m
@@ -0,0 +1,113 @@
+//
+//  LoginViewController.m
+//  Instagram
+//
+//  Created by Anna Thomas on 7/6/21.
+//
+
+#import "LoginViewController.h"
+#import <Parse/Parse.h>
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+
+
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+- (IBAction)didTapSignUp:(id)sender {
+    if([self.usernameTextField.text isEqual:@""]) {
+        [self showAlertAction:@"Username is empty"];
+    }
+    
+    if([self.passwordTextField.text isEqual:@""]) {
+        [self showAlertAction:@"Password is empty"];
+    }
+ 
+    [self registerUser];
+}
+
+- (IBAction)didTapLogIn:(id)sender {
+    if([self.usernameTextField.text isEqual:@""]) {
+        [self showAlertAction:@"Username is empty"];
+    }
+    
+    if([self.passwordTextField.text isEqual:@""]) {
+        [self showAlertAction:@"Password is empty"];
+    }
+    
+    [self loginUser];
+}
+
+-(void)showAlertAction:(NSString *) message {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Error" message:message preferredStyle:(UIAlertControllerStyleAlert)];
+    
+    // create an OK action
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) { }];
+    // add the OK action to the alert controller
+    [alert addAction:okAction];
+    
+    [self presentViewController:alert animated:YES completion:^{
+        // optional code for what happens after the alert controller has finished presenting
+    }];
+}
+
+- (void)registerUser {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameTextField.text;
+    //newUser.email = self.emailField.text;
+    newUser.password = self.passwordTextField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            // manually segue to logged in view
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
+}
+
+
+- (void)loginUser {
+    NSString *username = self.usernameTextField.text;
+    NSString *password = self.passwordTextField.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+        
+            // display view controller that needs to shown after successful login
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
