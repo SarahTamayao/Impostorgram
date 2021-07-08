50ca14f130799088498bfd76baf1b2ede8ba4151
athomas9195 <anna.tina.thomas@gmail.com>
2021-07-07 18:23:20 -0700

    ui

--- Instagram/ViewControllers/HomeViewController.m
+++ Instagram/ViewControllers/HomeViewController.m
@@ -38,6 +38,7 @@
     UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
     [refreshControl addTarget:self action:@selector(beginRefresh:) forControlEvents:UIControlEventValueChanged];
     [self.tableView insertSubview:refreshControl atIndex:0];
+    [refreshControl setTintColor:[UIColor lightGrayColor]];
     
     
     //table view delegate
@@ -87,7 +88,7 @@
     PFQuery *postQuery = [Post query];
     [postQuery orderByDescending:@"createdAt"];
     [postQuery includeKey:@"author"];
-    postQuery.limit = 20; 
+    postQuery.limit = 20;
 
     // fetch data asynchronously
     [postQuery findObjectsInBackgroundWithBlock:^(NSArray<Post *> * _Nullable posts, NSError * _Nullable error) {
